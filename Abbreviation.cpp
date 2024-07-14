029d289c59dfa5e0c902dc858867b6a1cca8172d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-14 09:25:01 +0900

    Update Abbreviation.cpp

--- Abbreviation.cpp
+++ Abbreviation.cpp
@@ -14,36 +14,31 @@ string rtrim(const string &);
  *  2. STRING b
  */
 
-string abbreviation(const std::string & a, const std::string & b)
+string abbreviation(string a, string b)
 {
-    const std::size_t a_size(a.size());
-    const std::size_t b_size(b.size());
+    const std::size_t aSize = a.size();
+    const std::size_t bSize = b.size();
     
-    std::vector<std::vector<bool>> 
-        can_abbre(a_size+1, std::vector<bool>(b_size+1,false));
-        
-    can_abbre[0][0] = true;
+    std::vector<std::vector<bool>> isAbbreviable(aSize + 1, std::vector<bool>(bSize + 1, false));
     
-    for (std::size_t i = 1; i <= a_size && std::islower(a[i-1]); ++i) 
-        can_abbre[i][0] = true ;
+    isAbbreviable[0][0] = true;
+    
+    for (std::size_t i = 1; i <= aSize && std::islower(a[i - 1]); ++i) 
+        isAbbreviable[i][0] = true;
 
-    for (std::size_t i = 1; i <= a_size; ++i)
-     {
-        for (std::size_t j = 1; j <= b_size; ++j)
-         {
-            if (std::islower(a[i-1]) && can_abbre[i-1][j])
-                can_abbre[i][j] = true;
+    for (std::size_t i = 1; i <= aSize; ++i)
+    {
+        for (std::size_t j = 1; j <= bSize; ++j)
+        {
+            if (std::islower(a[i - 1]) && isAbbreviable[i - 1][j])
+                isAbbreviable[i][j] = true;
                 
-            if (can_abbre[i-1][j-1] 
-                && ( std::toupper(a[i-1])==b[j-1] || a[i-1]==b[j-1]) )
-                can_abbre[i][j] = true;
+            if (isAbbreviable[i - 1][j - 1] && (std::toupper(a[i - 1]) == b[j - 1] || a[i - 1] == b[j - 1]))
+                isAbbreviable[i][j] = true;
         }
     }
     
-    if (can_abbre[a_size][b_size])
-        return "YES";
-    else
-        return "NO";
+    return isAbbreviable[aSize][bSize] ? "YES" : "NO";
 }
 
 
@@ -73,7 +68,6 @@ int main()
     return 0;
 }
 
-
 string ltrim(const string &str) {
     string s(str);
 
@@ -95,7 +89,3 @@ string rtrim(const string &str) {
 
     return s;
 }
-
-
-
-
