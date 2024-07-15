b1e99956657c289d81d9f26357eccd8721f55278
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-15 09:20:03 +0900

    Update Abbreviation.cs

--- Abbreviation.cs
+++ Abbreviation.cs
@@ -1,1 +1,32 @@
-Abbreviation
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'abbreviation' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING a
+     *  2. STRING b
+     */
+
+    public static string abbreviation(string a, string b)
+    {
+    }
+
+}
+
