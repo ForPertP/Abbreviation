e37b802b6334d31c038338317d986045e1dcff6a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-17 09:35:58 +0900

    Update Abbreviation.cs

--- Abbreviation.cs
+++ Abbreviation.cs
@@ -26,10 +26,32 @@ class Result
 
     public static string abbreviation(string a, string b)
     {
+        int aSize = a.Length;
+        int bSize = b.Length;
+        
+        bool[,] isAbbreviable = new bool[aSize + 1, bSize + 1];
+        
+        isAbbreviable[0, 0] = true;
+        
+        for (int i = 1; i <= aSize && char.IsLower(a[i - 1]); ++i)
+            isAbbreviable[i, 0] = true;
+
+        for (int i = 1; i <= aSize; ++i)
+        {
+            for (int j = 1; j <= bSize; ++j)
+            {
+                if (char.IsLower(a[i - 1]) && isAbbreviable[i - 1, j])
+                    isAbbreviable[i, j] = true;
+                    
+                if (isAbbreviable[i - 1, j - 1] && (char.ToUpper(a[i - 1]) == b[j - 1] || a[i - 1] == b[j - 1]))
+                    isAbbreviable[i, j] = true;
+            }
+        }
+        
+        return isAbbreviable[aSize, bSize] ? "YES" : "NO";
     }
 
 }
-
 
 class Solution
 {
@@ -54,4 +76,3 @@ class Solution
         textWriter.Close();
     }
 }
-
