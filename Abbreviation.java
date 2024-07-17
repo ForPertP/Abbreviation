a6d03062e7bd1dd10cc8f4281d095ba144867609
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-18 06:43:14 +0900

    Update Abbreviation.java

--- Abbreviation.java
+++ Abbreviation.java
@@ -1,1 +1,27 @@
-Java
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'abbreviation' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING a
+     *  2. STRING b
+     */
+
+    public static String abbreviation(String a, String b) {
+    }
+}
+
